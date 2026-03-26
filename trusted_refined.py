1c02c17be583b9df7674690d49922d8856e6024a
Erick3421 <erick.miyagi@sptech.school>
2026-03-26 17:03:16 -0300

    ajustes no refined.py

--- trusted_refined.py
+++ trusted_refined.py
@@ -1,50 +1,33 @@
 import pandas as pd
 import os
 
-def refine_data(trusted_path, refined_path):
+def refine_data():
+    df = pd.read_csv("./trusted/trusted_feriados.csv")
 
-    if not os.path.exists(refined_path):
-        os.makedirs(refined_path)
+    df["DATA"] = pd.to_datetime(df["DATA"])
 
-    trusted_file = os.path.join(trusted_path, "feriados_tratado.csv")
+    df["ANO"] = df["DATA"].dt.year
+    df["MES"] = df["DATA"].dt.month
+    df["DIA"] = df["DATA"].dt.day
+    df["NOME_MES"] = df["DATA"].dt.month_name()
 
-    refined_file = os.path.join(refined_path, "feriados_refined.csv")
-    estado_file = os.path.join(refined_path, "feriados_por_estado.csv")
-    mes_file = os.path.join(refined_path, "feriados_por_mes.csv")
-    categoria_file = os.path.join(refined_path, "feriados_por_categoria.csv")
-
-    df = pd.read_csv(trusted_file)
-
-    df["Data"] = pd.to_datetime(df["Data"])
-
-    df["Ano"] = df["Data"].dt.year
-    df["Mes"] = df["Data"].dt.month
-    df["Dia"] = df["Data"].dt.day
-    df["Nome_Mes"] = df["Data"].dt.month_name()
-
-    df["Feriado_Nacional"] = df["Categoria"].apply(
+    df["FERIADO_NACIONAL"] = df["TIPO"].apply(
         lambda x: "Sim" if x == "NACIONAL" else "Não"
     )
 
-    df.to_csv(refined_file, index=False)
+    df.to_csv("./refined/refined_feriados_nacional.csv", index=False)
 
-    feriados_estado = df.groupby("Estado").size().reset_index(name="Quantidade_Feriados")
-    feriados_estado.to_csv(estado_file, index=False)
+    feriados_estado = df.groupby("ESTADO").size().reset_index(name="Quantidade_Feriados")
+    feriados_estado.to_csv("./refined/refined_feriados_por_estado.csv", index=False)
 
-    feriados_mes = df.groupby("Mes").size().reset_index(name="Quantidade_Feriados")
-    feriados_mes.to_csv(mes_file, index=False)
+    feriados_mes = df.groupby("MES").size().reset_index(name="Quantidade_Feriados")
+    feriados_mes.to_csv("./refined/refined_feriados_por_mes.csv", index=False)
 
-    feriados_categoria = df.groupby("Categoria").size().reset_index(name="Quantidade_Feriados")
-    feriados_categoria.to_csv(categoria_file, index=False)
+    feriados_categoria = df.groupby("TIPO").size().reset_index(name="Quantidade_Feriados")
+    feriados_categoria.to_csv("./refined/refined_feriados_por_categoria.csv", index=False)
 
     print("Arquivos refined gerados")
 
 
 if __name__ == "__main__":
-
-    base_path = os.path.dirname(__file__)
-
-    trusted_directory = os.path.join(base_path, "trusted")
-    refined_directory = os.path.join(base_path, "refined")
-
-    refine_data(trusted_directory, refined_directory)
+    refine_data()
